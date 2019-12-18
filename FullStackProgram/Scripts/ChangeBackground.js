ec48c252acc9b447c370b75d697a250bed520f59
Stijn <s.marsman@expertswitch.nl>
2019-12-18 14:54:29 +0100

    Begin van de Indeed site, maar nog erg lelijk

---
+++ FullStackProgram/Scripts/ChangeBackground.js
@@ -0,0 +1,12 @@
+﻿FullStackProgram.backgroundButton = new function () {
+
+    this.Initialize = function () {
+        $('#backgroundButton').click(function () {
+            changBackground();
+        });
+    }
+
+    function changBackground() {
+        document.body.style.background = Math.floor(Math.random() * 16777215).toString(16);
+    }
+}
